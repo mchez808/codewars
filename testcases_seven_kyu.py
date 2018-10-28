02fd20d619c29fe42e98328fc1dcc61b641c1394
Mark Chesney <mark.chesney@gmail.com>
2018-10-27 18:35:19 -0700

    I could've sworn I just committed this change, with the 6 test cases

--- testcases_seven_kyu.py
+++ testcases_seven_kyu.py
@@ -5,8 +5,19 @@ from alpha_sequence import alpha_seq
 
 class AlphaSeqTestCase(unittest.TestCase):
     def test_one_letter(self):
-        self.assertEqual(alpha_seq('A'),'A')
-        self.assertEqual(alpha_seq('a'),'A')
+        self.assertEqual(alpha_seq('A'),
+                         'A')
+        self.assertEqual(alpha_seq('a'),
+                         'A')
+        self.assertEqual(alpha_seq('B'),
+                         'Bb')
+        self.assertEqual(alpha_seq('bb'),
+                         'Bb,Bb')
+        self.assertEqual(alpha_seq('Cbc'),
+                         'Bb,Ccc,Ccc')
+        self.assertEqual(alpha_seq('ZpglnRqenU'),
+                         'Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Zzzzzzzzzzzzzzzzzzzzzzzzzz'
+                        )
 
 
 if __name__ == '__main__':
