7cec8e66c2b7439d97bbf858f70a46c78a09246e
Mark Chesney <mark.chesney@gmail.com>
2018-10-31 08:15:04 -0700

    complete the 8-kyu 'abbreviate' kata

--- eight_kyu.py
+++ eight_kyu.py
@@ -53,12 +53,12 @@ def abbrevName(name):
     It should look like this:
 
     >>> abbrevName("Sam Harris")
-    "S.H"
+    'S.H'
 
     >>> abbrevName("Patrick Feeney")
-    "P.F"
+    'P.F'
 
     https://www.codewars.com/kata/abbreviate-a-two-word-name/train/python
     """
     #code away!
-    
+    return ".".join([name.split()[0][0], name.split()[1][0]]).upper()
