09910b15fd90a5c0b027e6c71bc1461add31f899
Mark Chesney <mark.chesney@gmail.com>
2018-11-03 14:40:58 -0700

    complete zero_fuel (easy) challenge

--- eight_kyu.py
+++ eight_kyu.py
@@ -61,3 +61,17 @@ def abbrevName(name):
     https://www.codewars.com/kata/abbreviate-a-two-word-name/train/python
     """
     return ".".join([name.split()[0][0], name.split()[1][0]]).upper()
+
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    """
+    You were camping with your friends far away from home, but when it's time to
+    go back, you realize that you fuel is running out and the nearest pump is
+    50 miles away! You know that on average, your car runs on about 25 miles per
+    gallon. There are 2 gallons left. Considering these factors, write a
+    function that tells you if it is possible to get to the pump or not.
+    Function should return true if it is possible and false if not.
+
+    https://www.codewars.com/kata/5861d28f124b35723e00005e/train/python
+    """
+    return distance_to_pump <= mpg*fuel_left
