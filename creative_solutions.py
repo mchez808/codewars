2ad5707c3fbfd97a310998ec531dc9602b05daa2
Mark Chesney <mark.chesney@gmail.com>
2018-11-12 15:17:40 -0800

    add binary_array_to_number() 7_kyu (very fun too)

--- creative_solutions.py
+++ creative_solutions.py
@@ -1,5 +1,39 @@
+# This file contains clever and advanced solutions that are not my own work,
+# but they are the work I strive to produce. 
 
-## 6 kyu
+# 7 kyu
+
+def alpha_seq(string):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    output = ''
+    for s in ''.join(sorted((string.lower()))):
+        output += s.upper()
+        output += s * alpha.find(s)
+        output += ','
+    return output.strip(',')
+
+######## THE CREATIVE SOLUTION FROM CODEWARS ########
+# def alpha_seq(s):
+#     return ",".join( (c * (ord(c)-96)).capitalize() for c in sorted(s.lower()) )
+#####################################################
+
+
+def binary_array_to_number(arr):
+    sum = 0
+
+    leading_decimal = len(arr)
+    for exponent_subtrahend, binary_digit in enumerate(arr):
+        sum += binary_digit * (2**(leading_decimal-exponent_subtrahend-1))
+    return sum
+
+######## THE CREATIVE SOLUTION FROM CODEWARS ########
+    # return int("".join(map(str, arr)), 2)
+######## THE CREATIVE SOLUTION FROM CODEWARS ########
+    # return int(''.join(str(a) for a in arr), 2)
+#####################################################
+
+
+# 6 kyu
 
 def reverse_alternate(s):
   words = s.split()
@@ -19,16 +53,3 @@ https://www.codewars.com/kata/invert-values/train/python
 
 def invert(inputlist):
     return [-n for n in inputlist]
-
-
-def alpha_seq(string):
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    output = ''
-    for s in ''.join(sorted((string.lower()))):
-        output += s.upper()
-        output += s * alpha.find(s)
-        output += ','
-    return output.strip(',')
-
-# def alpha_seq(s):
-#     return ",".join( (c * (ord(c)-96)).capitalize() for c in sorted(s.lower()) )
